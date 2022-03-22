e09eb4746922d8dabfb31f9151e65480dc939e48
mathieu <mathecool22@gmail.com>
2022-03-22 17:00:16 -0400

    deux options pour le carousel

--- src/components/LeCarousel.js
+++ src/components/LeCarousel.js
@@ -7,8 +7,9 @@ import image2 from "../images/carou2.webp";
 import image3 from "../images/carou3.webp";
 import image4 from "../images/carou4.webp";
 import image5 from "../images/carou5.webp";
-// import image6 from "../images/carou6.webp";
-
+// Core modules imports are same as usual
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import { GrNext } from "react-icons/gr";
 const LeCarousel = (props) => {
 	const [marge, setMarge] = useState(0);
@@ -64,17 +65,42 @@ const LeCarousel = (props) => {
 	};
 
 	return (
-		<div className={classes.contCarouPerso}>
-			<div className={classes.prevBtn} onClick={prevSlide}>
-				{props.queryMobile && <GrNext size="75px" />}
-				{props.queryDesktop && <GrNext size="50px" />}
+		<>
+			<div className={classes.contCarouPerso}>
+				<div className={classes.prevBtn} onClick={prevSlide}>
+					{props.queryMobile && <GrNext size="75px" />}
+					{props.queryDesktop && <GrNext size="50px" />}
+				</div>
+				<div className={classes.fenetre}>{bande}</div>
+				<div className={classes.nextBtn} onClick={nextSlide}>
+					{props.queryMobile && <GrNext size="75px" />}
+					{props.queryDesktop && <GrNext size="50px" />}
+				</div>
 			</div>
-			<div className={classes.fenetre}>{bande}</div>
-			<div className={classes.nextBtn} onClick={nextSlide}>
-				{props.queryMobile && <GrNext size="75px" />}
-				{props.queryDesktop && <GrNext size="50px" />}
-			</div>
-		</div>
+
+			<Swiper
+				spaceBetween={100}
+				slidesPerView={1}
+				onSlideChange={() => console.log("slide change")}
+				onSwiper={(swiper) => console.log(swiper)}
+			>
+				<SwiperSlide>
+					<img className={classes.imgDeSwiper} src={image1} alt="" />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img className={classes.imgDeSwiper} src={image2} alt="" />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img className={classes.imgDeSwiper} src={image3} alt="" />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img className={classes.imgDeSwiper} src={image4} alt="" />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img className={classes.imgDeSwiper} src={image5} alt="" />
+				</SwiperSlide>
+			</Swiper>
+		</>
 	);
 };
 export default LeCarousel;
