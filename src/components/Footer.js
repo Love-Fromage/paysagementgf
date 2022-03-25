12d9977cf9d69adb66333bb6da7e1ecafaf2f5f2
mathieu <mathecool22@gmail.com>
2022-03-25 16:18:10 -0400

    majorite du site fini reste a gosser un peu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,32 +4,61 @@ import facebook from "../images/facebook.webp";
 
 const Footer = (props) => {
 	return (
-		<div className={classes.contFooter}>
-			<div className={classes.footerVert}>
-				<div className={classes.adresse}>
-					<p>3495 rue de l'aronia</p>
-					<p>Saint-Bruno-de-Montarville</p>
-					<p>QC J3V 0A4</p>
+		<div>
+			{props.queryMobile && (
+				<div className={classes.contFooter}>
+					<div className={classes.footerVert}>
+						<div className={classes.adresse}>
+							<p>3495 rue de l'aronia</p>
+							<p>Saint-Bruno-de-Montarville</p>
+							<p>QC J3V 0A4</p>
+						</div>
+						<div className={classes.email}>
+							<p>fernando@paysagementgf.com</p>
+							<a href="tel:5146387692">514 638 7692</a>
+							<a href="https://www.facebook.com/PaysagementGF/">
+								<img src={facebook} alt="facebook" />
+							</a>
+						</div>
+					</div>
+					<div className={classes.footerRouge}>
+						<div className={classes.titre}>
+							<h1>
+								paysagement gf les <br />
+								portugais
+							</h1>
+							<a href="https://blackduckagency.com">
+								© 2022 par Black Duck Agency
+							</a>
+						</div>
+					</div>
 				</div>
-				<div className={classes.email}>
-					<p>fernando@paysagementgf.com</p>
-					<a href="tel:5146387692">514 638 7692</a>
-					<a href="https://www.facebook.com/PaysagementGF/">
-						<img src={facebook} alt="facebook" />
-					</a>
+			)}
+			{props.queryDesktop && (
+				<div className={classes.contFooterD}>
+					<div className={classes.footerVert}>
+						<a href="https://www.facebook.com/PaysagementGF/">
+							<img src={facebook} alt="facebook" />
+						</a>
+						<div className={classes.adresse}>
+							<p>3495 rue de l'aronia</p>
+							<p>Saint-Bruno-de-Montarville, QC J3V 0A4</p>
+						</div>
+						<div className={classes.email}>
+							<p>fernando@paysagementgf.com</p>
+							<a href="tel:5146387692">514 638 7692</a>
+						</div>
+					</div>
+					<div className={classes.footerRouge}>
+						<div className={classes.titre}>
+							<h1>paysagement gf les portugais</h1>
+							<a href="https://blackduckagency.com">
+								© 2022 par Black Duck Agency
+							</a>
+						</div>
+					</div>
 				</div>
-			</div>
-			<div className={classes.footerRouge}>
-				<div className={classes.titre}>
-					<h1>
-						paysagement gf les <br />
-						portugais
-					</h1>
-					<a href="https://blackduckagency.com">
-						© 2022 par Black Duck Agency
-					</a>
-				</div>
-			</div>
+			)}
 		</div>
 	);
 };
